6bf02508e1f202e08e38b748ed7c4bbde9e3ad93
rogsersjw <Jonathan.W.Rogers@gmail.com>
2017-03-06 22:28:39 -0800

    Finished most of the airfoil math. Now to start adding the interactivity

--- client/main.js
+++ client/main.js
@@ -4,8 +4,7 @@ import { PritchardAirfoil, default_pritchard_params } from './pritchard_airfoil'
 import './main.html';
 
 var p_airfoil = new PritchardAirfoil(default_pritchard_params);
-
-var points = [{x: 10, y: 50}, {x: 10, y: 10}, {x: 100, y: 10}, {x: 180, y: 190}, {x: 50, y: 50}];
+// var points = [{x: 10, y: 50}, {x: 10, y: 10}, {x: 100, y: 10}, {x: 180, y: 190}, {x: 50, y: 50}];
 
 Template.hello.onCreated(function helloOnCreated() {
     // counter starts at 0
@@ -26,186 +25,281 @@ Template.hello.events({
     },
 });
 
-Template.airfoil_section_svg.rendered = function sectionOnCreated() {
+Template.airfoil_sec_svg.events({
+    // 'change #cx_input'(event, instance){
+    //     console.log(event.target.value);
+    //     p_airfoil.cx = event.target.value;
+    //     p_airfoil.calc_airfoil();
+    //     console.log(p_airfoil.cx);
+    // }
+});
 
-    var lut = [
-        [1]
-    ];
+Template.airfoil_sec_svg.rendered = function sectionOnCreated() {
 
-    var binomial = function(n) {
-        var s, nextRow, prev;
-        while (n >= lut.length){
-            nextRow = [];
-            nextRow.push(1);
-            prev = lut.length-1;
-            for (i=1; i <= prev; i++){
-                nextRow.push(lut[prev][i-1] + lut[prev][i]);
-            }
-            nextRow.push(1);
-            lut.push(nextRow);
-        }
-        return lut[n];
-    };
+    $('#cx_input').val(p_airfoil.cx);
 
-    console.log(binomial(6));
-    console.log(lut);
-
-    var var_func = function(ws, t){
-        var x_val = 0;
-        var n = ws.length;
-        var bin = binomial(n);
-        for (i=0; i < n; i++){
-            // if (t == 0){
-            //     console.log(ws[i])
-            //     console.log(bin[i])
-            //     console.log(Math.pow(1-t,n-i))
-            //     console.log(Math.pow(t,i))
-            //     console.log('sum=' + ws[i]*bin[i]*Math.pow(1-t,n-i)*Math.pow(t,i))
-            // }
-            x_val += ws[i]*bin[i]*Math.pow(1-t,n-i)*Math.pow(t,i);
-        }
-        return x_val;
-    };
-
-    var width = 800;
-    var height = 600;
-    var point_radius = 10
+    var width = 500;
+    var height = 500;
+    var point_radius = 0.1;
     var dragging = false;
 
     // Variables to keep track of the current scale factor and translation when zooming/panning.
-    var current_scale = 1.0
+    var current_scale = 1.0;
     var current_translate = [0, 0];
     // Variables to keep track of current x & y viewable areas
     // We'll use the scale and translate above later to manipulate these to know exactly what is showing.
     var x_constraints = [0, width];
     var y_constraints = [0, height];
 
+    // Set up the SVG area with width and height
     var svg = d3.select('#airfoil_section');
     svg.attr('width', width);
     svg.attr('height', height);
 
+    // Set up background SVG
     var bg_rect = svg.append('rect')
         .attr('width', width)
         .attr('height', height)
         .attr('fill', 'gray')
 
-    var zoom_group = svg.append('g');
+    // Append a group to the SVG
+    // This group will be used to zoom in/out
+    var zoom_group = svg.append('g')
+        .attr('id', 'zoom_group');
 
+    // Set up zoom behavior.
+    // Listen for the zoom event on the background, then apply it to the zoom_group (all airfoil info)
     bg_rect.call(d3.behavior.zoom().on("zoom", function () {
-            current_translate = d3.event.translate;
-            current_scale = d3.event.scale;
+        current_translate = d3.event.translate;
+        current_scale = d3.event.scale;
 
-            zoom_group.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+        zoom_group.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
 
-            x_constraints = [-current_translate[0]/current_scale, (width-current_translate[0])/current_scale]
-            y_constraints = [-current_translate[1]/current_scale, (height-current_translate[1])/current_scale]
+        x_constraints = [-current_translate[0]/current_scale, (width-current_translate[0])/current_scale]
+        y_constraints = [-current_translate[1]/current_scale, (height-current_translate[1])/current_scale]
     }));
 
-    // svg.selectAll('circle').attr('fill','red');
-    var num_points = 50;
-    var ts = []
-    for (i=0; i<num_points; i++){
-        ts.push(i*(1.0/(num_points-1)));
-    }
+    // Create X and Y scales
+    var svg_ratio = width/height;
+    var x_scale = d3.scale.linear()
+        .domain(p_airfoil.get_extents())
+        .range([0, width]);
+    var y_scale = d3.scale.linear()
+        .domain(p_airfoil.get_extents())
+        .range([height*svg_ratio, 0]);
 
+    // Create the line to do the paths around the airfoil.
     var line = d3.svg.line()
         .interpolate('linear')
         .x(function(d) {
-                var ws = [];
-                for (i = 0; i < points.length; i++) {
-                    ws.push(points[i].x);
-                }
-                return var_func(ws, d);
-            })
+            return x_scale(d.x);
+        })
         .y(function(d) {
-            var ws = [];
-            for (i = 0; i < points.length; i++) {
-                ws.push(points[i].y);
-            }
-            return var_func(ws, d);
+            return y_scale(d.y);
         });
 
-    // line = d3.svg.line()
-    //     .x(function(d){return d.x})
-    //     .y(function(d){return d.y})
-    //     .interpolate('linear');
-
+    // Add the leading edge line to the zoomgroup
     zoom_group.selectAll('path.line')
-        .data([ts])
+        .data([p_airfoil.le_pts, p_airfoil.te_pts, p_airfoil.ss_pts, p_airfoil.ps_pts, p_airfoil.thrt_pts])
         .enter()
         .append('path')
         .attr('class','line')
         .attr('d', function(d) {return line(d)})
         .attr("stroke", "blue")
-        .attr("stroke-width", 2)
+        .attr("stroke-width", 1.0)
         .attr("fill", "none");
 
-    // svg.selectAll('path.line')
-    //     .data([points])
-    //     .attr('d', function(d){
-    //         return line(d);
-    //     });
-
-    drag_point = d3.behavior.drag()
-        .on('dragstart', function(d) {
-            this.parentNode.appendChild(this);
-        })
-        .on('drag', function(d, i) {
-            var e, x, y;
-            // console.log(i);
-            e = d3.event
-            x = d.x + e.dx;
-            y = d.y + e.dy;
-
-            if (x - point_radius >= x_constraints[0] && x + point_radius <= x_constraints[1]
-                && y - point_radius >= y_constraints[0] && y + point_radius <= y_constraints[1]) {
-                // change cx, cy only after we know it's within
-                // bounds...this gives smoother movement around the
-                // boundaries
-                cx = x, cy=y;
-                d.x = x;
-                d.y = y;
-                points[i].x = x;
-                points[i].y = y;
-                d3.select(this).attr({cx: cx, cy: cy});
-
-                zoom_group.selectAll('path.line')
-                    .data([ts])
-                    .attr('class','line')
-                    .attr('d', function(d) {return line(d)});
-            }
-        })
-
     var circ = zoom_group.selectAll('circle.points')
-        .data(points)
+        .data([p_airfoil.pt1, p_airfoil.pt2, p_airfoil.pt3, p_airfoil.pt4, p_airfoil.pt5])
         .enter().append('circle')
         .attr('class', 'points')
-        .attr('r', point_radius)
-        .attr('cx', function(d) {return d.x})
-        .attr('cy', function(d) {return d.y})
-        .attr('fill', 'green')
-        .call(drag_point);
+        .attr('r', 2)
+        .attr('cx', function(d) {return x_scale(d.x)})
+        .attr('cy', function(d) {return y_scale(d.y)})
+        .attr('fill', 'red');
 
+    d3.select('#cx_input')
+        .on('change', function(){
+            p_airfoil.cx = Number($('#cx_input').val());
+            p_airfoil.calc_airfoil();
+
+            zoom_group.selectAll('path.line')
+                .data([p_airfoil.le_pts, p_airfoil.te_pts, p_airfoil.ss_pts, p_airfoil.ps_pts, p_airfoil.thrt_pts])
+                .attr('d', function(d) {return line(d)})
+                .attr("stroke", "blue")
+                .attr("stroke-width", 1.0)
+                .attr("fill", "none");
+
+            zoom_group.selectAll('circle.points')
+                .data([p_airfoil.pt1, p_airfoil.pt2, p_airfoil.pt3, p_airfoil.pt4, p_airfoil.pt5])
+                .attr('r', 2)
+                .attr('cx', function(d) {return x_scale(d.x)})
+                .attr('cy', function(d) {return y_scale(d.y)})
+                .attr('fill', 'red');
+            return false;
+        });
+
+    // zoom_group.attr("transform", "translate(" + current_translate + ")" + " scale(" + current_scale + ")");
 };
 
-Template.zoom_test.rendered = function sectionOnCreated() {
-
-    var width = 500;
-    var height = 500;
-
-    var svg = d3.select('#zoom_test');
-    svg.attr('width', width);
-    svg.attr('height', height);
-
-
-    var svg = svg.call(d3.behavior.zoom().on("zoom", function () {
-            svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
-        }))
-        .append("g")
-
-    svg.append("circle")
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .attr("r", 50)
-        .style("fill", "blue")
-};
+// Template.airfoil_section_svg.rendered = function sectionOnCreated() {
+//
+//     var lut = [
+//         [1]
+//     ];
+//
+//     var binomial = function(n) {
+//         var s, nextRow, prev;
+//         while (n >= lut.length){
+//             nextRow = [];
+//             nextRow.push(1);
+//             prev = lut.length-1;
+//             for (i=1; i <= prev; i++){
+//                 nextRow.push(lut[prev][i-1] + lut[prev][i]);
+//             }
+//             nextRow.push(1);
+//             lut.push(nextRow);
+//         }
+//         return lut[n];
+//     };
+//
+//     console.log(binomial(6));
+//     console.log(lut);
+//
+//     var var_func = function(ws, t){
+//         var x_val = 0;
+//         var n = ws.length;
+//         var bin = binomial(n);
+//         for (i=0; i < n; i++){
+//             // if (t == 0){
+//             //     console.log(ws[i])
+//             //     console.log(bin[i])
+//             //     console.log(Math.pow(1-t,n-i))
+//             //     console.log(Math.pow(t,i))
+//             //     console.log('sum=' + ws[i]*bin[i]*Math.pow(1-t,n-i)*Math.pow(t,i))
+//             // }
+//             x_val += ws[i]*bin[i]*Math.pow(1-t,n-i)*Math.pow(t,i);
+//         }
+//         return x_val;
+//     };
+//
+//     var width = 800;
+//     var height = 600;
+//     var point_radius = 10
+//     var dragging = false;
+//
+//     // Variables to keep track of the current scale factor and translation when zooming/panning.
+//     var current_scale = 1.0
+//     var current_translate = [0, 0];
+//     // Variables to keep track of current x & y viewable areas
+//     // We'll use the scale and translate above later to manipulate these to know exactly what is showing.
+//     var x_constraints = [0, width];
+//     var y_constraints = [0, height];
+//
+//     var svg = d3.select('#airfoil_section');
+//     svg.attr('width', width);
+//     svg.attr('height', height);
+//
+//     var bg_rect = svg.append('rect')
+//         .attr('width', width)
+//         .attr('height', height)
+//         .attr('fill', 'gray')
+//
+//     var zoom_group = svg.append('g');
+//
+//     bg_rect.call(d3.behavior.zoom().on("zoom", function () {
+//             current_translate = d3.event.translate;
+//             current_scale = d3.event.scale;
+//
+//             zoom_group.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+//
+//             x_constraints = [-current_translate[0]/current_scale, (width-current_translate[0])/current_scale]
+//             y_constraints = [-current_translate[1]/current_scale, (height-current_translate[1])/current_scale]
+//     }));
+//
+//     // svg.selectAll('circle').attr('fill','red');
+//     var num_points = 50;
+//     var ts = []
+//     for (i=0; i<num_points; i++){
+//         ts.push(i*(1.0/(num_points-1)));
+//     }
+//
+//     var line = d3.svg.line()
+//         .interpolate('linear')
+//         .x(function(d) {
+//                 var ws = [];
+//                 for (i = 0; i < points.length; i++) {
+//                     ws.push(points[i].x);
+//                 }
+//                 return var_func(ws, d);
+//             })
+//         .y(function(d) {
+//             var ws = [];
+//             for (i = 0; i < points.length; i++) {
+//                 ws.push(points[i].y);
+//             }
+//             return var_func(ws, d);
+//         });
+//
+//     // line = d3.svg.line()
+//     //     .x(function(d){return d.x})
+//     //     .y(function(d){return d.y})
+//     //     .interpolate('linear');
+//
+//     zoom_group.selectAll('path.line')
+//         .data([ts])
+//         .enter()
+//         .append('path')
+//         .attr('class','line')
+//         .attr('d', function(d) {return line(d)})
+//         .attr("stroke", "blue")
+//         .attr("stroke-width", 2)
+//         .attr("fill", "none");
+//
+//     // svg.selectAll('path.line')
+//     //     .data([points])
+//     //     .attr('d', function(d){
+//     //         return line(d);
+//     //     });
+//
+//     drag_point = d3.behavior.drag()
+//         .on('dragstart', function(d) {
+//             this.parentNode.appendChild(this);
+//         })
+//         .on('drag', function(d, i) {
+//             var e, x, y;
+//             // console.log(i);
+//             e = d3.event
+//             x = d.x + e.dx;
+//             y = d.y + e.dy;
+//
+//             if (x - point_radius >= x_constraints[0] && x + point_radius <= x_constraints[1]
+//                 && y - point_radius >= y_constraints[0] && y + point_radius <= y_constraints[1]) {
+//                 // change cx, cy only after we know it's within
+//                 // bounds...this gives smoother movement around the
+//                 // boundaries
+//                 cx = x, cy=y;
+//                 d.x = x;
+//                 d.y = y;
+//                 points[i].x = x;
+//                 points[i].y = y;
+//                 d3.select(this).attr({cx: cx, cy: cy});
+//
+//                 zoom_group.selectAll('path.line')
+//                     .data([ts])
+//                     .attr('class','line')
+//                     .attr('d', function(d) {return line(d)});
+//             }
+//         })
+//
+//     var circ = zoom_group.selectAll('circle.points')
+//         .data(points)
+//         .enter().append('circle')
+//         .attr('class', 'points')
+//         .attr('r', point_radius)
+//         .attr('cx', function(d) {return d.x})
+//         .attr('cy', function(d) {return d.y})
+//         .attr('fill', 'green')
+//         .call(drag_point);
+//
+// };
--- client/pritchard_airfoil.js
+++ client/pritchard_airfoil.js
@@ -36,7 +36,7 @@ var default_pritchard_params = {
     ct: 0.591,
     uct: 6.5*Math.PI/180.0,
     b1: 35.0*Math.PI/180.0,
-    db1: 18.0*Math.PI/180.0,
+    db1: 16.0*Math.PI/180.0,
     rle: 0.031,
     b2: -57.0*Math.PI/180.0,
     rte: 0.016,
@@ -69,6 +69,159 @@ function PritchardAirfoil(parameters) {
     this.lift = function() {
         return 2*this.s()/this.c()*0.5*(Math.cos(this.b1) + Math.cos(this.b2))*(Math.tan(this.b1) - Math.tan(this.b2));
     };
+    this.db2 = this.uct;
+
+    // Geom Vars
+    this.num_points = 50;
+    this.le_pts = null;
+    this.te_pts = null;
+    this.ps_pts = null;
+    this.ss_pts = null;
+    this.thrt_pts = null;
+    this.pt1 = null;
+    this.pt2 = null;
+    this.pt3 = null;
+    this.pt4 = null;
+    this.pt5 = null;
+
+    // Complete the airfoil by calculating all variables.
+    this.calc_airfoil();
+}
+
+PritchardAirfoil.prototype.calc_airfoil = function() {
+    this.find_db2();
+
+    this.calc_pt1();
+    this.calc_pt2();
+    this.calc_pt3();
+    this.calc_pt4();
+    this.calc_pt5();
+
+    this.calc_le();
+    this.calc_te();
+    this.calc_ss();
+    this.calc_ps();
+    this.calc_thrt();
+};
+
+PritchardAirfoil.prototype.calc_pt1 = function() {
+    var b1 = this.b2 - this.db2/2.0;
+    var x1 = this.cx - this.rte*(1 + Math.sin(b1));
+    var y1 = this.rte*Math.cos(b1);
+    this.pt1 = {x: x1, y: y1, b: b1};
+};
+
+PritchardAirfoil.prototype.calc_pt2 = function() {
+    var b2 = this.b2 - this.db2/2.0 + this.uct;
+    var x2 = this.cx - this.rte + (this.o + this.rte)*Math.sin(b2);
+    var y2 = 2.0*Math.PI*this.r/this.nb - (this.o + this.rte)*Math.cos(b2);
+    this.pt2 = {x: x2, y: y2, b: b2};
+};
+
+PritchardAirfoil.prototype.calc_pt3 = function() {
+    var b3 = this.b1 + this.db1/2.0;
+    var x3 = this.rle*(1 - Math.sin(b3));
+    var y3 = this.ct + this.rle*Math.cos(b3);
+    this.pt3 = {x: x3, y: y3, b: b3};
+};
+
+PritchardAirfoil.prototype.calc_pt4 = function() {
+    var b4 = this.b1 - this.db1/2.0;
+    var x4 = this.rle*(1 + Math.sin(b4));
+    var y4 = this.ct - this.rle*Math.cos(b4);
+    this.pt4 = {x: x4, y: y4, b: b4};
+};
+
+PritchardAirfoil.prototype.calc_pt5 = function() {
+    var b5 = this.b2 + this.db2/2.0;
+    var x5 = this.cx - this.rte*(1 - Math.sin(b5));
+    var y5 = -this.rte*Math.cos(b5);
+    this.pt5 = {x: x5, y: y5, b: b5};
+};
+
+PritchardAirfoil.prototype.calc_le = function() {
+    var t_start = this.b1 + this.db1/2.0 + Math.PI/2.0;
+    var t_end = 2*Math.PI + (this.b1 - this.db1/2.0 - Math.PI/2.0);
+    var dt = (t_end - t_start)/(this.num_points - 1);
+    var pts = [];
+    var new_x, new_y;
+    for (i=0; i<this.num_points; i++){
+        new_x = this.rle*Math.cos(t_start + dt*i) + this.rle;
+        new_y = this.rle*Math.sin(t_start + dt*i) + this.ct;
+        pts.push({x: new_x, y: new_y});
+    }
+    this.le_pts = pts;
+};
+
+PritchardAirfoil.prototype.calc_te = function() {
+    var t_start = this.b2 - this.db2/2.0 + Math.PI/2.0;
+    var t_end = this.b2 + this.db1/2.0 - Math.PI/2.0;
+    var dt = (t_end - t_start)/(this.num_points - 1);
+    var pts = [];
+    var new_x, new_y;
+    for (i=0; i<this.num_points; i++){
+        new_x = this.rte*Math.cos(t_start + dt*i) + this.cx - this.rte;
+        new_y = this.rte*Math.sin(t_start + dt*i);
+        pts.push({x: new_x, y: new_y});
+    }
+    this.te_pts = pts;
+};
+
+PritchardAirfoil.prototype.calc_ps = function() {
+    this.ps_pts = cubic_points_from_endpoints(this.num_points, this.pt4, this.pt5)
+};
+
+PritchardAirfoil.prototype.calc_ss = function() {
+    this.ss_pts = cubic_points_from_endpoints(this.num_points, this.pt3, this.pt2);
+};
+
+PritchardAirfoil.prototype.calc_thrt = function() {
+    var thrt_funcs = arc_func_from_pts(this.pt1, this.pt2);
+    var dt = 1.0/(this.num_points - 1);
+    var pts = []
+    for (i=0; i < this.num_points; i++){
+        pts.push({x: thrt_funcs.x(i*dt), y: thrt_funcs.y(i*dt)});
+    }
+    this.thrt_pts = pts
+};
+
+PritchardAirfoil.prototype.db2_error_func = function(){
+    var args = [this.b2, this.cx, this.rte, this.uct, this.o, this.r, this.nb];
+    var f = function(db2, args) {
+        b2 = args[0];
+        cx = args[1];
+        rte = args[2];
+        uct = args[3];
+        o = args[4];
+        r = args[5];
+        nb = args[6];
+        var b1 = b2 - db2/2.0;
+        var x1 = cx - rte*(1 + Math.sin(b1));
+        var y1 = rte*Math.cos(b1);
+        var pt1 = {x: x1, y: y1, b: b1};
+        var b2 = b2 - db2/2.0 + uct;
+        var x2 = cx - rte + (o + rte)*Math.sin(b2);
+        var y2 = 2.0*Math.PI*r/nb - (o + rte)*Math.cos(b2);
+        var pt2 = {x: x2, y: y2, b: b2};
+        var arc_func = arc_func_from_pts(pt1, pt2);
+        return (arc_func.y(0.0) - pt1.y) + (arc_func.y(1.0) - pt2.y);
+    }
+    return function(db2) {return f(db2, args)};
+}
+
+PritchardAirfoil.prototype.find_db2 = function() {
+    var f = this.db2_error_func();
+    [converged, value, sequence] = newton(this.db2, f, this.uct/10.0, this.uct*3.0);
+    // console.log(value);
+    if (converged){
+        this.db2 = value;
+    } else {
+        this.db2 = this.uct;
+    }
+};
+
+PritchardAirfoil.prototype.get_extents = function() {
+    return [-2.0*(this.rte), d3.max([1.2*this.ct, 1.1*(this.cx + this.rte)])];
 }
 
 // to_string - Output the information from independent/dependent vars
@@ -96,9 +249,106 @@ PritchardAirfoil.prototype.toString = function() {
     return msg;
 };
 
-//
-// PritchardAirfoil.calc_airfoil = function() {
-//
-// };
+// Other Helper Functions
+var cubic_points_from_endpoints = function(num_points, pt1, pt2){
+    var d = (Math.tan(pt1.b) + Math.tan(pt2.b))/Math.pow(pt1.x - pt2.x, 2) -
+        2*(pt1.y - pt2.y)/Math.pow(pt1.x - pt2.x, 3);
+    var c = (pt1.y - pt2.y)/Math.pow(pt1.x - pt2.x, 2) - Math.tan(pt2.b)/(pt1.x - pt2.x) -
+        d*(pt1.x + 2*pt2.x);
+    var b = Math.tan(pt2.b) - 2*c*pt2.x - 3*d*Math.pow(pt2.x, 2);
+    var a = pt2.y - b*pt2.x - c*Math.pow(pt2.x, 2) - d*Math.pow(pt2.x, 3);
+
+    var x_step = (pt2.x - pt1.x)/(num_points - 1);
+    var pts = [];
+    var new_y, new_x;
+    for (i = 0; i < num_points; i++){
+        new_x = pt1.x + x_step*i;
+        new_y = a + b*new_x + c*Math.pow(new_x, 2) + d*Math.pow(new_x, 3);
+        pts.push({x: new_x, y: new_y});
+    }
+    return pts;
+};
+
+var circ_params_from_pts = function(pt1, pt2){
+    // pt1 and pt2 must be of form {x: x_val, y: y_val, b: tangent angle}
+    var m1, m2, b1, b2, cx, cy, r;
+    m1 = -1.0/Math.tan(pt1.b);
+    m2 = -1.0/Math.tan(pt2.b);
+    b1 = pt1.y - m1*pt1.x;
+    b2 = pt2.y - m1*pt2.x;
+    cx = (b2-b1)/(m1-m2);
+    cy = m1*cx+b1;
+    r = Math.sqrt(Math.pow(pt1.x-cx, 2) + Math.pow(pt1.y-cy, 2));
+    return [r, cx, cy];
+};
+
+var arc_func_from_pts = function(pt1, pt2){
+    var r, cx, cy;
+    [r, cx, cy] = circ_params_from_pts(pt1, pt2);
+    var ang1 = Math.atan2(pt1.y - cy, pt1.x - cx);
+    var ang2 = Math.atan2(pt2.y - cy, pt2.x - cx);
+    if (ang2 > ang1){
+        return arc_funcs(r, cx, cy, ang1, ang2);
+    } else {
+        return arc_funcs(r, cx, cy, ang2, ang1);
+    }
+};
+
+var circ_funcs = function(r, cx, cy){
+  return {
+      x: function(t) {return r*Math.cos(t) + cx },
+      y: function(t) {return r*Math.sin(t) + cy }
+  };
+};
+
+var arc_funcs = function(r, cx, cy, t1, t2){
+    return {
+        x: function(t) {return r*Math.cos(t*(t2-t1) + t1) + cx },
+        y: function(t) {return r*Math.sin(t*(t2-t1) + t1) + cy }
+    };
+}
+
+// Newton's Method
+//a function to compute the data for a line
+var line = function(f, a, deltax) {
+    var deltay = f(a + deltax) - f(a);
+    return [a, f(a), deltax, deltay];
+};
+
+//a function for returning the zero
+//apply the output of line to this
+var xinter = function(a, c, deltax, deltay) {
+    return (-c * deltax) / deltay + a;
+};
+
+//core Newton's method
+var rawNewton = function(guess, f, leftBound, rightBound, dx, minXdist, minYdist, exit) {
+    //paste default: 1e-10, 1e-10, 1e-10, 10
+    var sequencePoints = [[guess, f(guess)]];
+    var lineData, prevGuess, y;
+    y = f(guess);
+    //newton loop
+    for (exit; exit > 0; exit -= 1) {
+        lineData = line(f, guess, dx);
+        prevGuess = guess;
+        y = lineData[1];
+        guess = xinter.apply(this, lineData);
+        if ((guess < leftBound - dx) || (guess > rightBound + dx)) {
+            return [false, "Out of Bounds:" + guess, sequencePoints];
+        }
+        //add (a,f(a))
+        sequencePoints.push([guess, f(guess)]);
+        if ((Math.abs(guess - prevGuess) < minXdist) && (Math.abs(y) < minYdist)) {
+            return [true, guess, sequencePoints];
+        }
+    }
+    //did not meet the required distances
+    return [false, "Did not converge", sequencePoints];
+};
+
+//a wrapper for list of defaults
+var newton = function(g, f, lb, rb) {
+    return rawNewton.call(this, g, f, lb, rb, 1e-10, 1e-10, 1e-10, 10);
+};
 
 export { PritchardAirfoil, default_pritchard_params };
